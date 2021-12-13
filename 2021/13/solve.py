fe27ef53d37c2f8cad8dccb57ad19d88e08ce7ed
Jacek Suchenia <jacek.suchenia@ocado.com>
2021-12-13 14:19:38 +0100

    Yet another refactor of 2021/13 - to learn Python

--- 2021/13/solve.py
+++ 2021/13/solve.py
@@ -4,35 +4,26 @@ from itertools import groupby
 def dump(points):
     for y, row in groupby(sorted(points, key=lambda x: x[1]), lambda x:x[1]):
         allX = [point[0] for point in row]
-        print(''.join(['#' if i in allX else '.' for i in range(0,max(allX)+1)]))
-
-def executeCommand(points, command):
-    val = int(command[13:])
-    pos = 1 if command[11] == 'y' else 0
-
-    for point in points:
-        if point[pos] > val:
-            diff = 2 * (point[pos] - val)
-            yield (point[0] - (diff if pos == 0 else 0), point[1] - (diff if pos == 1 else 0))
-        elif point[pos] < val:
-            yield point
+        print(''.join(['#' if i in allX else ' ' for i in range(0,max(allX)+1)]))
 
 def parse(data):
     (dataLines, commandLines) = data.split("\n\n")
     commands = commandLines.splitlines()
-    points = set([tuple([int(s) for s in line.split(",")]) for line in dataLines.splitlines()])
+    points = set(tuple(map(int, line.split(","))) for line in dataLines.splitlines())
     return points,commands
 
 def doTask(data, runAll=False):
     points, commands = parse(data)
 
     for cmd in commands:
-        points = set(executeCommand(points, cmd))
+        foldValue = int(cmd[13:])
+        axis = 1 if cmd[11] == 'y' else 0
+        points = {tuple(min(2*foldValue - val, val) if i==axis else val for i, val in enumerate(point)) for point in points}
         if not runAll: break
 
     result = len(points)
     print("EX{}> Finally we have {} points".format("2" if runAll else "1", result))
-    print("Result", points)
+    print("Result", result)
 
     if runAll: dump(points)
     return result
@@ -44,5 +35,5 @@ if __name__ == "__main__":
     assert doTask(test) == 17
     assert doTask(input) == 827
 
-    assert doTask(test, True) == 16
-    assert doTask(input, True) == 104
+    assert doTask(test, runAll=True) == 16
+    assert doTask(input, runAll=True) == 104
