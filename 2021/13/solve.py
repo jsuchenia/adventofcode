1b04ca5f47cf808a1dcb948a8566c1b28d2930a1
Jacek Suchenia <jacek.suchenia@ocado.com>
2021-12-13 13:51:47 +0100

    Refactor task 2021/13

--- 2021/13/solve.py
+++ 2021/13/solve.py
@@ -1,80 +1,48 @@
 #!/usr/local/bin/python3
-from collections import defaultdict
+from itertools import groupby
 
 def dump(points):
-    d = defaultdict(list)
+    for y, row in groupby(sorted(points, key=lambda x: x[1]), lambda x:x[1]):
+        allX = [point[0] for point in row]
+        print(''.join(['#' if i in allX else '.' for i in range(0,max(allX)+1)]))
 
-    for point in points:
-        d[point[1]].append(point)
-
-    maxY = max(d.keys())
-
-    for y in range(0,maxY+1):
-        if y in d:
-            row = d[y]
-            allX = [point[0] for point in row]
-            maxX = max(allX)
-            chars = ['#' if i in allX else '.' for i in range(0,maxX+1)]
-            print(''.join(chars))
-        else:
-            print("")
 def executeCommand(points, command):
     val = int(command[13:])
-    newPoints = set()
+    pos = 1 if command[11] == 'y' else 0
 
-    if command.startswith("fold along y="):
-        for point in points:
-            if point[1] > val:
-                diff = 2* (point[1] - val)
-                print("For y={} we have newY={}".format(point[1], point[1] - diff))
-                newPoints.add((point[0], point[1] - diff))
-            elif point[1] == val:
-                continue
-            else:
-                newPoints.add(point)
-    elif command.startswith("fold along x="):
-        for point in points:
-            if point[0] > val:
-                diff = 2*(point[0] - val)
-                print("For x={} we have newx={}".format(point[0], point[0] - diff))
-                newPoints.add((point[0]-diff, point[1]))
-            elif point[0] == val:
-                continue
-            else:
-                newPoints.add(point)
+    for point in points:
+        if point[pos] > val:
+            diff = 2 * (point[pos] - val)
+            yield (point[0] - (diff if pos == 0 else 0), point[1] - (diff if pos == 1 else 0))
+        elif point[pos] < val:
+            yield point
 
-    return newPoints
-
-def ex1(data, runAll=False):
-    points = set()
-
+def parse(data):
     (dataLines, commandLines) = data.split("\n\n")
     commands = commandLines.splitlines()
+    points = set([tuple([int(s) for s in line.split(",")]) for line in dataLines.splitlines()])
+    return points,commands
 
-    for line in dataLines.splitlines():
-        strX,strY = line.split(",")
-        points.add((int(strX), int(strY)))
+def doTask(data, runAll=False):
+    points, commands = parse(data)
 
-    if runAll:
-        for cmd in commands:
-            points = executeCommand(points, cmd)
-    else:
-        points = executeCommand(points, commands[0])
+    for cmd in commands:
+        points = set(executeCommand(points, cmd))
+        if not runAll: break
 
     result = len(points)
     print("EX{}> Finally we have {} points".format("2" if runAll else "1", result))
     print("Result", points)
 
-    if runAll:
-        dump(points)
+    if runAll: dump(points)
     return result
 
 if __name__ == "__main__":
     test = open("test.txt", "r").read()
     input = open("data.txt", "r").read()
 
-    assert ex1(test) == 17
-    assert ex1(input) == 827
+    assert doTask(test) == 17
+    assert doTask(input) == 827
 
-    assert ex1(test, True) == 16
-    assert ex1(input, True) == 104
+    assert doTask(test, True) == 16
+    assert doTask(input, True) == 104
