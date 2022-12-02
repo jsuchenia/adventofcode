b0f43461080b919bb5b6e828da426c692e59c773
Jacek <jacek@suchenia.pl>
2022-12-02 18:31:19 +0100

    Simplify (overcomplicate?) day 2

--- 2022/2/count_points.py
+++ 2022/2/count_points.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 from pprint import pprint
 
+# Common:
 # A for Rock, B for Paper, and C for Scissors (opponent)
 # X for Rock, Y for Paper, and Z for Scissors (my move)
 
+# Points
 # 0 if you lost, 3 if the round was a draw, and 6 if you won
 # 1 for Rock, 2 for Paper, and 3 for Scissors
-score1_table = {
-    'A': {'X': 3 + 1, 'Y': 6 + 2, 'Z': 0 + 3},
-    'B': {'X': 0 + 1, 'Y': 3 + 2, 'Z': 6 + 3},
-    'C': {'X': 6 + 1, 'Y': 0 + 2, 'Z': 3 + 3}
-}
 
+# Case 2:
 # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
-score2_table = {
-    'A': {'X': 3, 'Y': 1 + 3, 'Z': 2 + 6},
-    'B': {'X': 1, 'Y': 2 + 3, 'Z': 3 + 6},
-    'C': {'X': 2, 'Y': 3 + 3, 'Z': 1 + 6}
-}
+
+# Points calculated from a matrix (then matrix simplified)
+points1 = [3, 6, 0]
+points2 = [3, 1, 2]
 
 
 def get_scores(file_name: str):
     with open(file_name, "r") as f:
         data = [line.strip().split(" ") for line in f.readlines()]
+    num = [(ord(x[0]) - ord('A'), ord(x[1]) - ord('X')) for x in data]
 
-    score1 = sum([score1_table[opponent][you] for opponent, you in data])
-    score2 = sum([score2_table[opponent][you] for opponent, you in data])
+    score1 = sum([points1[(n[1] - n[0]) % 3] + n[1] + 1 for n in num])
+    score2 = sum([points2[(n[1] + n[0]) % 3] + 3 * n[1] for n in num])
 
     return score1, score2
 
