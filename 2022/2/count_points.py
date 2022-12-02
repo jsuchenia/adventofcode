66387d592c0e9b1724deb1c95f66125491a5dfd6
Jacek <jacek@suchenia.pl>
2022-12-02 07:53:23 +0100

    Simplification of a code + asserts

--- 2022/2/count_points.py
+++ 2022/2/count_points.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python
 from pprint import pprint
 
-
-def get_data(file_name: str):
-    with open(file_name, "r") as f:
-        return [line.strip().split(" ") for line in f.readlines()]
-
-
 # A for Rock, B for Paper, and C for Scissors (opponent)
 # X for Rock, Y for Paper, and Z for Scissors (my move)
 
 # 0 if you lost, 3 if the round was a draw, and 6 if you won
-
-score1_table = {
-    'A': {'X': 3, 'Y': 6, 'Z': 0},
-    'B': {'X': 0, 'Y': 3, 'Z': 6},
-    'C': {'X': 6, 'Y': 0, 'Z': 3}
-}
-
 # 1 for Rock, 2 for Paper, and 3 for Scissors
-move1_score = {'X': 1, 'Y': 2, 'Z': 3}
+score1_table = {
+    'A': {'X': 3 + 1, 'Y': 6 + 2, 'Z': 0 + 3},
+    'B': {'X': 0 + 1, 'Y': 3 + 2, 'Z': 6 + 3},
+    'C': {'X': 6 + 1, 'Y': 0 + 2, 'Z': 3 + 3}
+}
 
 # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
-move2_score = {'X': 0, 'Y': 3, 'Z': 6}
-
 score2_table = {
-    'A': {'X': 3, 'Y': 1, 'Z': 2},
-    'B': {'X': 1, 'Y': 2, 'Z': 3},
-    'C': {'X': 2, 'Y': 3, 'Z': 1}
+    'A': {'X': 3, 'Y': 1 + 3, 'Z': 2 + 6},
+    'B': {'X': 1, 'Y': 2 + 3, 'Z': 3 + 6},
+    'C': {'X': 2, 'Y': 3 + 3, 'Z': 1 + 6}
 }
 
+
+def get_scores(file_name: str):
+    with open(file_name, "r") as f:
+        data = [line.strip().split(" ") for line in f.readlines()]
+
+    score1 = sum([score1_table[opponent][you] for opponent, you in data])
+    score2 = sum([score2_table[opponent][you] for opponent, you in data])
+
+    return score1, score2
+
+
 if __name__ == "__main__":
-    data = get_data("data1.txt")
-    pprint(data)
-
-    score1 = [score1_table[opponent][you] + move1_score[you] for opponent, you in data]
-    pprint(score1)
-    pprint(sum(score1))
-
-    score2 = [score2_table[opponent][you] + move2_score[you] for opponent, you in data]
-    pprint(score2)
-    pprint(sum(score2))
+    assert get_scores("example.txt") == (15, 12)
+    pprint(get_scores("data1.txt"))
