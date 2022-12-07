7e9619ca005a0a2d9e42615dc8a5d0840eab281d
Jacek <jacek@suchenia.pl>
2022-12-07 07:33:45 +0100

    Code simplification

--- 2022/7/no_space_left.py
+++ 2022/7/no_space_left.py
@@ -2,73 +2,58 @@ def build_data(file_name: str):
     with open(file_name) as f:
         data = f.read().split('\n')
 
-    root_dir = {}
-    stack = None
-    curr_dir = None
+    root, stack, cwd = {}, None, None
 
     for line in data:
-        if line.startswith("$ "):
-            if line.startswith("$ cd "):
-                new_dir = line[5:]
-                if new_dir == "/":
-                    stack = [root_dir]
-                    curr_dir = root_dir
-                elif new_dir == "..":
-                    curr_dir = stack.pop()
-                elif new_dir in curr_dir:
-                    stack.append(curr_dir)
-                    curr_dir = curr_dir[new_dir]
-                else:
-                    raise Exception("Unknown directory " + new_dir)
+        if line.startswith("$ ls"):
+            continue
+        elif line.startswith("$ cd "):
+            cd_param = line[5:]
+            if cd_param == "/":
+                stack = [root]
+                cwd = root
+            elif cd_param == "..":
+                cwd = stack.pop()
+            elif cd_param in cwd:
+                stack.append(cwd)
+                cwd = cwd[cd_param]
+        elif line.startswith("dir "):
+            dir_name = line[4:]
+            if dir_name not in cwd:
+                cwd[dir_name] = {}
         else:
-            if line.startswith("dir "):
-                dir_name = line[4:]
-                if dir_name not in curr_dir:
-                    curr_dir[dir_name] = {}
-                else:
-                    raise Exception("Diplicated directory " + dir_name)
-            else:
-                size, f = line.split(" ")
-                curr_dir[f] = int(size)
-    return root_dir
+            size, f = line.split(" ")
+            cwd[f] = int(size)
+    return root
 
 
-def count_size(dir_name: str, dir: dict) -> (int, list):
-    size = 0
-    bigdirs = []
+def count_size(name: str, element, dir_sizes: list) -> int:
+    if isinstance(element, int)
+        return element
 
-    for name, value in dir.items():
-        if isinstance(value, dict):
-            new_name = dir_name + "/" + name
-            dir_size, dirs = count_size(new_name, value)
-            bigdirs += dirs
-            bigdirs.append((new_name, dir_size))
-            size += dir_size
-            print(f"dif {new_name} with size {size}")
-        elif isinstance(value, int):
-            size += value
-        else:
-            raise Exception(f"Wrong entry in dirmap {name} {value}")
-    return size, bigdirs
+    size = sum([count_size(f"{name}/{key}", value, dir_sizes) for key, value in element.items()])
+
+    print(f"dif {name} with size {size}")
+    dir_sizes.append((name, size))
+    return size
 
 
-MAX_LIMIT = 40000000
+MAX_LIMIT = 40_000_000
 
 
-def traverse(file_name: str, size_limit: int = 100000):
+def traverse(file_name: str, size_limit: int = 100_000):
     root_dir = build_data(file_name)
-    root_size, dir_sizes = count_size("", root_dir)
+    dir_sizes = []
+    root_size = count_size("", root_dir, dir_sizes)
 
     small_sizes = sum([size for name, size in dir_sizes if size <= size_limit])
-    print(small_sizes)
+    print(f"Total weight of small dirs is {small_sizes}")
 
-    to_delete = root_size - MAX_LIMIT
+    found = [size for name, size in dir_sizes if size >= root_size - MAX_LIMIT]
+    found.sort()
+    print(f"Smallest one that fit is {found[0]}")
 
-    choosen = [size for name, size in dir_sizes if size >= to_delete]
-    choosen.sort()
-    print(f"Smallest one that fit {to_delete} is {choosen[0]}")
-
-    return small_sizes, choosen[0]
+    return small_sizes, found[0]
 
 
 if __name__ == "__main__":
