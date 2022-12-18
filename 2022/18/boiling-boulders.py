7e2738f1d4c6898808117db8f3402ddd9616cfcc
Jacek <jacek@suchenia.pl>
2022-12-18 09:42:56 +0100

    AoC 2022 Day 18 - fixed naming

--- 2022/18/boiling-boulders.py
+++ 2022/18/boiling-boulders.py
@@ -9,7 +9,7 @@ def read_data(filename: str) -> frozenset[tuple]:
 PATTERN = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
 
-def generate_cloud(cubes: frozenset[tuple]) -> set[tuple]:
+def generate_water(cubes: frozenset[tuple]) -> set[tuple]:
     limits = []
     for i in range(3):
         values = [cube[i] for cube in cubes]
@@ -17,35 +17,35 @@ def generate_cloud(cubes: frozenset[tuple]) -> set[tuple]:
 
     q = deque([(limits[0][0], limits[1][0], limits[2][0])])
 
-    cloud = set()
+    water = set()
 
     while q:
         air = q.popleft()
         for dx, dy, dz in PATTERN:
-            new_air = (air[0] + dx, air[1] + dy, air[2] + dz)
+            pos = (air[0] + dx, air[1] + dy, air[2] + dz)
 
-            if not (limits[0][0] <= new_air[0] <= limits[0][1]
-                    and limits[1][0] <= new_air[1] <= limits[1][1]
-                    and limits[2][0] <= new_air[2] <= limits[2][1]):
+            if not (limits[0][0] <= pos[0] <= limits[0][1]
+                    and limits[1][0] <= pos[1] <= limits[1][1]
+                    and limits[2][0] <= pos[2] <= limits[2][1]):
                 continue
 
-            if (new_air not in cloud) and (new_air not in cubes):
-                cloud.add(new_air)
-                q.append(new_air)
+            if (pos not in water) and (pos not in cubes):
+                water.add(pos)
+                q.append(pos)
 
-    return cloud
+    return water
 
 
 def count_surfaces(filename: str) -> (int, int):
     cubes = read_data(filename)
-    outside_air = generate_cloud(cubes)
+    water = generate_water(cubes)
     result1 = result2 = 0
 
     for cube in cubes:
         for dx, dy, dz in PATTERN:
             if (cube[0] + dx, cube[1] + dy, cube[2] + dz) not in cubes:
                 result1 += 1
-            if (cube[0] + dx, cube[1] + dy, cube[2] + dz) in outside_air:
+            if (cube[0] + dx, cube[1] + dy, cube[2] + dz) in water:
                 result2 += 1
 
     print(f"{result1=} {result2=}")
