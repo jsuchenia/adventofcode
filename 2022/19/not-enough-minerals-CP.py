2c7eb670a1f9a7eab748768d29284ef2510521ad
Jacek <jacek@suchenia.pl>
2022-12-19 18:50:26 +0100

    AoC 2022 - day 19 - CP updates

--- 2022/19/not-enough-minerals-CP.py
+++ 2022/19/not-enough-minerals-CP.py
@@ -15,8 +15,8 @@ def read_data(filename: str):
 
 # DFS like - go deep and check how much goede we earn
 def simul(id, ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geode_cost_ore, geode_cost_obs, limit):
-    # Cost of each deicsion - each row is a decision, each colum is a cost of it
-    # 0 - is just wait, 1 - buy ORE harvester, 2 - buy CLAY harvester, 3 - buy obsydian harvester, 4 - buy geode harvester
+    # Cost of each decision - each row is a decision, each colum is a cost of it
+    # 0 - is just wait, 1 - buy ORE harvester, 2 - buy CLAY harvester, 3 - buy obsidian harvester, 4 - buy geode harvester
     cost_array = cpm_array([
         [0, 0, 0, 0],
         [ore_cost_ore, 0, 0, 0],
@@ -24,39 +24,40 @@ def simul(id, ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geode_co
         [obs_cost_ore, obs_cost_clay, 0, 0],
         [geode_cost_ore, 0, geode_cost_obs, 0],
     ])
-    decision = intvar(0, 4, shape=(1, limit + 1), name="decision")
+    decision = intvar(0, 4, shape=(limit + 1, 1), name="decision")
 
     # Number of bots that we have "active" AT the end current round
-    bots = intvar(0, 100, shape=(4, limit + 1), name="bots")
+    bots = intvar(0, 100, shape=(limit + 1, 4), name="bots")
     # Resources that we have in each round
-    resources = intvar(0, 500, shape=(4, limit + 1), name="resources")
+    resources = intvar(0, 500, shape=(limit + 1, 4), name="resources")
 
     model = Model()
 
-    for res_id in range(4):
-        # Initial values for step 0 - only 1 ORE harvester and 0 resources
-        model += (bots[res_id, 0] == (1 if res_id == 0 else 0))
-        model += (resources[res_id, 0] == 0)
+    # Initial values for step 0 - only 1 ORE harvester and 0 resources
+    model += bots[0] == (1, 0, 0, 0)
+    model += resources[0] == (0, 0, 0, 0)
 
+    for res_id in range(4):
         for step in range(1, limit + 1):
-            what_to_buy = decision[0, step]
+            what_to_buy = decision[step, 0]
 
             # To buy a harvester we need to have enough resources
-            model += (resources[res_id, step - 1] >= cost_array[what_to_buy, res_id])
+            model += resources[step - 1, res_id] >= cost_array[what_to_buy, res_id]
 
             # This is how we calculate resources at the end of this round
             resource_cost_in_step = cost_array[what_to_buy, res_id]
-            model += (resources[res_id, step] == resources[res_id, step - 1] + bots[res_id, step - 1] - resource_cost_in_step)
+            model += resources[step, res_id] == resources[step - 1, res_id] + bots[step - 1, res_id] - resource_cost_in_step
 
             # Number of bots will be increased by a decision
             # In CPMpy we have to convert if statements to math calculation
-            extra_bot_for_resource = ((what_to_buy - 1) == res_id)
-            model += (bots[res_id, step] == (bots[res_id, step - 1]) + extra_bot_for_resource)
+            extra_bot_for_resource = (what_to_buy - 1) == res_id
+            model += bots[step, res_id] == bots[step - 1, res_id] + extra_bot_for_resource
 
-    model.maximize(resources[3][limit])  # Objective: maximize the number of geodes in the end
+    model.maximize(resources[limit, 3])  # Objective: maximize the number of geodes in the end
+
     if model.solve():
         print(f"  [{id}] {model.status()}")
-        return resources[3][limit].value()
+        return resources[limit, 3].value()
     raise ValueError("No solution for this clue!")
 
 
