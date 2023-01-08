729ec4765723ad077bb73cc9b8b29854f0f096cd
Jacek <jacek@suchenia.pl>
2023-01-08 10:18:32 +0100

    Missing pytest for 2021/25

--- 2021/25/sea_cucumber_test.py
+++ 2021/25/sea_cucumber_test.py
@@ -1,12 +1,10 @@
 #!/usr/local/bin/python3
 from copy import deepcopy
 
-def ex1(data):
+def ex1(filename):
+    data = open(filename, "r").read()
     map = [list(line) for line in data.splitlines()]
     step = 0
-
-    for line in map:
-        print(''.join(line))
 
     while True:
         moved = False
@@ -15,31 +13,28 @@ def ex1(data):
 
         for y in range(len(map)):
             for x in range(len(map[y])):
-                if map[y][x] == '>' and map[y][(x+1) % len(map[y])] == '.':
+                if map[y][x] == '>' and map[y][(x + 1) % len(map[y])] == '.':
                     newmap[y][x] = '.'
-                    newmap[y][(x+1) % len(map[y])] = '>'
+                    newmap[y][(x + 1) % len(map[y])] = '>'
                     moved = True
         map = deepcopy(newmap)
         for y in range(len(map)):
             for x in range(len(map[y])):
-                if map[y][x] == 'v' and map[(y+1) % len(map)][x] == '.':
+                if map[y][x] == 'v' and map[(y + 1) % len(map)][x] == '.':
                     newmap[y][x] = '.'
-                    newmap[(y+1) % len(map)][x] = 'v'
+                    newmap[(y + 1) % len(map)][x] = 'v'
                     moved = True
 
         map = deepcopy(newmap)
-        print("-------------", step)
-        for line in map:
-            print(''.join(line))
+        # print("-------------", step)
+        # for line in map:
+        #     print(''.join(line))
         if not moved:
             print("Step not moved", step)
             return step
-        else:
-            print("Step moved - next loop", step)
 
-if __name__ == "__main__":
-    test = open("test.txt", "r").read()
-    data = open("data.txt", "r").read()
+def test_game_of_live_test():
+    assert ex1("test.txt") == 58
 
-    assert ex1(test) == 58
-    assert ex1(data) == 563
+def test_game_of_live_data():
+    assert ex1("data.txt") == 563
