a598176b98105c09d1c942bd79c1f719630251ff
Jacek <jacek@suchenia.pl>
2023-12-10 23:02:02 +0100

    Implement some ideas saw on Reddit

---
+++ 2023/10/pipe_maze_math_test.py
@@ -0,0 +1,78 @@
+# Pipe Maze - https://adventofcode.com/2023/day/10
+# From Reddit: https://www.reddit.com/r/adventofcode/comments/18evyu9/comment/kcso138/
+#    https://en.wikipedia.org/wiki/Shoelace_formula
+#    https://en.wikipedia.org/wiki/Pick%27s_theorem
+
+type MazePoint = tuple[int, int]
+type Connections = dict[MazePoint, list[MazePoint]]
+
+
+def get_data(filename: str) -> list[str]:
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def parse_connections(lines: list[str]) -> tuple[Connections, MazePoint]:
+    connections = dict()
+    start = None
+    for y, row in enumerate(lines):
+        for x, cell in enumerate(row.strip()):
+            if cell == "|":
+                connections[(x, y)] = [(x, y - 1), (x, y + 1)]
+            elif cell == "-":
+                connections[(x, y)] = [(x - 1, y), (x + 1, y)]
+            elif cell == "L":
+                connections[(x, y)] = [(x, y - 1), (x + 1, y)]
+            elif cell == "J":
+                connections[(x, y)] = [(x, y - 1), (x - 1, y)]
+            elif cell == "7":
+                connections[(x, y)] = [(x, y + 1), (x - 1, y)]
+            elif cell == "F":
+                connections[(x, y)] = [(x, y + 1), (x + 1, y)]
+            elif cell == "S":
+                start = (x, y)
+
+    connections[start] = [src for src, conn in connections.items() for dst in conn if dst == start]
+    return connections, start
+
+
+def compute_loop(connections: Connections, start: MazePoint) -> list[MazePoint]:
+    point = start
+    results = [start]
+    prev_node = None
+    while True:
+        next_nodes = [p for p in connections[point] if p != prev_node]
+        prev_node = point
+        point = next_nodes[0]
+        if point == start:
+            break
+        results.append(point)
+    return results
+
+
+def q2(filename: str) -> int:
+    lines = get_data(filename)
+    connections, start = parse_connections(lines)
+    loop = compute_loop(connections, start)
+
+    # Shoelace formula
+    s = 0
+    for i in range(len(loop)):
+        n_1 = loop[i]
+        n_2 = loop[(i + 1) % len(loop)]
+        x_1, y_1 = n_1
+        x_2, y_2 = n_2
+        s += x_1 * y_2 - y_1 * x_2
+
+    area = abs(s / 2)
+
+    # Pick's theroem
+    print(area - len(loop) / 2 + 1)
+    return int(area - len(loop) / 2 + 1)
+
+
+def test_q2():
+    assert q2("test2.txt") == 4
+    assert q2("test3.txt") == 8
+    assert q2("data.txt") == 467
---
+++ 2023/10/pipe_maze_shapely_test.py
@@ -0,0 +1,83 @@
+# Pipe Maze - https://adventofcode.com/2023/day/10
+# Implementation in shapely library - something new
+
+from shapely import Point, Polygon
+
+type MazePoint = tuple[int, int]
+type Connections = dict[MazePoint, list[MazePoint]]
+
+
+def get_data(filename: str) -> list[str]:
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def parse_connections(lines: list[str]) -> tuple[Connections, MazePoint]:
+    connections = dict()
+    start = None
+    for y, row in enumerate(lines):
+        for x, cell in enumerate(row.strip()):
+            if cell == "|":
+                connections[(x, y)] = [(x, y - 1), (x, y + 1)]
+            elif cell == "-":
+                connections[(x, y)] = [(x - 1, y), (x + 1, y)]
+            elif cell == "L":
+                connections[(x, y)] = [(x, y - 1), (x + 1, y)]
+            elif cell == "J":
+                connections[(x, y)] = [(x, y - 1), (x - 1, y)]
+            elif cell == "7":
+                connections[(x, y)] = [(x, y + 1), (x - 1, y)]
+            elif cell == "F":
+                connections[(x, y)] = [(x, y + 1), (x + 1, y)]
+            elif cell == "S":
+                start = (x, y)
+
+    connections[start] = [src for src, conn in connections.items() for dst in conn if dst == start]
+    return connections, start
+
+
+def compute_loop(connections: Connections, start: MazePoint) -> list[MazePoint]:
+    point = start
+    results = [start]
+    prev_node = None
+    while True:
+        next_nodes = [p for p in connections[point] if p != prev_node]
+        prev_node = point
+        point = next_nodes[0]
+        if point == start:
+            break
+        results.append(point)
+    return results
+
+
+def q1(filename: str) -> int:
+    lines = get_data(filename)
+    connections, start = parse_connections(lines)
+    points = [Point(p) for p in compute_loop(connections, start)]
+    p = Polygon(points)
+    return p.boundary.length // 2
+
+
+def q2(filename: str) -> int:
+    lines = get_data(filename)
+    connections, start = parse_connections(lines)
+    points = [Point(p) for p in compute_loop(connections, start)]
+    p = Polygon(points)
+    interior = p.area - p.boundary.length / 2 + 1
+
+    # # It takes 55 seconds, but we can check if point is in a polygon
+    # interior_points = [(x, y) for y in range(len(lines)) for x in range(len(lines[0])) if p.contains(Point(x, y))]
+    # print(f"{len(interior_points)=}")
+    return interior
+
+
+def test_q1():
+    assert q1("test.txt") == 8
+    assert q1("data.txt") == 6927
+
+
+def test_q2():
+    assert q2("test2.txt") == 4
+    assert q2("test3.txt") == 8
+    assert q2("data.txt") == 467
