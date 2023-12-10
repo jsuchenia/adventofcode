14cfc7cbd4bc4428a2f030d992b6861520133e64
Jacek <jacek@suchenia.pl>
2023-12-10 12:53:41 +0100

    Day 10 - Cleanup

--- 2023/10/pipe_maze_test.py
+++ 2023/10/pipe_maze_test.py
@@ -1,5 +1,8 @@
 # Pipe Maze - https://adventofcode.com/2023/day/10
-from collections import defaultdict, deque
+from collections import deque
+
+type Point = tuple[int, int]
+type Connections = dict[Point, list[Point]]
 
 
 def get_data(filename: str) -> list[str]:
@@ -8,81 +11,60 @@ def get_data(filename: str) -> list[str]:
     return lines
 
 
-def get_connections(filename: str):
-    data = get_data(filename)
-    connections = defaultdict(list)
+def parse_connections(lines: list[str]) -> tuple[Connections, Point]:
+    connections = dict()
     start = None
-    for y, row in enumerate(data):
+    for y, row in enumerate(lines):
         for x, cell in enumerate(row.strip()):
             if cell == "|":
-                connections[(x, y)].append((x, y - 1))
-                connections[(x, y)].append((x, y + 1))
+                connections[(x, y)] = [(x, y - 1), (x, y + 1)]
             elif cell == "-":
-                connections[(x, y)].append((x - 1, y))
-                connections[(x, y)].append((x + 1, y))
-
+                connections[(x, y)] = [(x - 1, y), (x + 1, y)]
             elif cell == "L":
-                connections[(x, y)].append((x, y - 1))
-                connections[(x, y)].append((x + 1, y))
-
+                connections[(x, y)] = [(x, y - 1), (x + 1, y)]
             elif cell == "J":
-                connections[(x, y)].append((x, y - 1))
-                connections[(x, y)].append((x - 1, y))
-
+                connections[(x, y)] = [(x, y - 1), (x - 1, y)]
             elif cell == "7":
-                connections[(x, y)].append((x, y + 1))
-                connections[(x, y)].append((x - 1, y))
-
+                connections[(x, y)] = [(x, y + 1), (x - 1, y)]
             elif cell == "F":
-                connections[(x, y)].append((x, y + 1))
-                connections[(x, y)].append((x + 1, y))
+                connections[(x, y)] = [(x, y + 1), (x + 1, y)]
             elif cell == "S":
                 start = (x, y)
 
-    start_connections = []
-    for src, conn in connections.items():
-        for dst in conn:
-            if dst == start:
-                start_connections.append(src)
-
-    connections[start] = start_connections
-
+    connections[start] = [src for src, conn in connections.items() for dst in conn if dst == start]
     return connections, start
 
 
-def q1(filename: str) -> int:
-    connections, start = get_connections(filename)
-
-    loop = set()
+def compute_loop(connections, start) -> set[Point]:
+    visited = set()
     q = deque()
     q.append(start)
 
+    # BFS
     while q:
         cell = q.popleft()
-        loop.add(cell)
+        visited.add(cell)
         for next_cell in connections[cell]:
-            if next_cell not in loop:
+            if next_cell not in visited:
                 q.append(next_cell)
+
+    return visited
+
+
+def q1(filename: str) -> int:
+    lines = get_data(filename)
+    connections, start = parse_connections(lines)
+    loop = compute_loop(connections, start)
     return len(loop) // 2
 
 
 def q2(filename: str) -> int:
-    connections, start = get_connections(filename)
+    lines = get_data(filename)
+    connections, start = parse_connections(lines)
+    loop = compute_loop(connections, start)
 
-    loop = set()
-    q = deque()
-    q.append(start)
-
-    while q:
-        cell = q.popleft()
-        loop.add(cell)
-        for next_cell in connections[cell]:
-            if next_cell not in loop:
-                q.append(next_cell)
-
-    data = get_data(filename)
     inside = set()
-    for y, row in enumerate(data):
+    for y, row in enumerate(lines):
         enclosed = False
         horizontal_start_char = None
         for x, cell in enumerate(row.strip()):
@@ -95,17 +77,12 @@ def q2(filename: str) -> int:
                     if horizontal_start_char == "L":
                         enclosed = not enclosed
                     horizontal_start_char = None
-                elif cell == "J":
+                elif cell == "J" or cell == "S":  # Adjusted for my input data where S seems to be J
                     if horizontal_start_char == "F":
                         enclosed = not enclosed
                     horizontal_start_char = None
-                elif cell == "S":
-                    if connections[start][1][0] - start[0]:
-                        enclosed = not enclosed
-                    horizontal_start_char = None
-            else:
-                if enclosed:
-                    inside.add((x, y))
+            elif enclosed:
+                inside.add((x, y))
     return len(inside)
 
 
