1ad9bcdb2d2959b2ac7883ce139f8a9616067bc6
Jacek <jacek@suchenia.pl>
2023-12-11 08:06:31 +0100

    Add missing types, update dependencies

--- 2023/11/cosmic_expansion_test.py
+++ 2023/11/cosmic_expansion_test.py
@@ -10,14 +10,14 @@ def get_data(filename: str) -> set[Point]:
     return {(x, y) for y, line in enumerate(lines) for x, char in enumerate(line) if char == "#"}
 
 
-def expand_galaxy(galaxy: set[Point], scaling_factor) -> set[Point]:
+def expand_galaxy(galaxy: set[Point], scaling_factor: int) -> set[Point]:
     all_x = [x for x, y in galaxy]
     all_y = [y for x, y in galaxy]
 
     empty_x = [x for x in range(max(all_x)) if x not in all_x]
     empty_y = [y for y in range(max(all_y)) if y not in all_y]
 
-    def expand(x, y):
+    def expand(x: int, y: int) -> Point:
         dx = len([ex for ex in empty_x if ex < x])
         dy = len([ey for ey in empty_y if ey < y])
         return x + dx * (scaling_factor - 1), y + dy * (scaling_factor - 1)
@@ -25,7 +25,7 @@ def expand_galaxy(galaxy: set[Point], scaling_factor) -> set[Point]:
     return {expand(x, y) for x, y in galaxy}
 
 
-def distance(p1, p2):
+def distance(p1: Point, p2: Point) -> int:
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
@@ -37,7 +37,7 @@ def q1(filename: str) -> int:
     return sum(distances)
 
 
-def q2(filename: str, scaling_factor) -> int:
+def q2(filename: str, scaling_factor: int) -> int:
     galaxy = get_data(filename)
     galaxy = expand_galaxy(galaxy, scaling_factor=scaling_factor)
 
