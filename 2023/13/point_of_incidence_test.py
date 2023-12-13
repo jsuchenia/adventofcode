8d6151958becf33e22eecb908c27aca3d73e87e5
Jacek <jacek@suchenia.pl>
2023-12-13 08:24:09 +0100

    Day 13 - Cleanup

--- 2023/13/point_of_incidence_test.py
+++ 2023/13/point_of_incidence_test.py
@@ -1,19 +1,19 @@
 # Point of Incidence - https://adventofcode.com/2023/day/13
 
 
-def get_data(filename: str) -> list[str]:
+def get_mirrors(filename: str) -> list[str]:
     with open(filename) as f:
         return f.read().split("\n\n")
 
-def count_different(line1: str, line2: str) -> int:
-    return sum(1 if c1 != c2 else 0 for c1, c2 in zip(line1, line2))
+def count_diff_chars(*lines) -> int:
+    return sum(1 if c1 != c2 else 0 for c1, c2 in zip(*lines))
 
-def get_reflection(data: list[str], diff: int) -> int:
+def get_reflection_position(data: list[str], diff: int) -> int:
     for i in range(1, len(data)):
         d1 = reversed(data[:i])
         d2 = data[i:2 * i]
 
-        if sum(count_different(line1, line2) for line1, line2 in zip(d1, d2)) == diff:
+        if sum(count_diff_chars(*lines) for lines in zip(d1, d2)) == diff:
             return i
     return 0
 
@@ -21,10 +21,10 @@ def get_score(mirror: str, diff: int) -> int:
     lines = mirror.splitlines()
     columns = list(zip(*lines))
 
-    return get_reflection(columns, diff) + get_reflection(lines, diff) * 100
+    return get_reflection_position(columns, diff) + get_reflection_position(lines, diff) * 100
 
 def q1(filename: str, diff) -> int:
-    mirrors = get_data(filename)
+    mirrors = get_mirrors(filename)
     return sum(get_score(mirror, diff) for mirror in mirrors)
 
 def test_q1():
