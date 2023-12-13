196d28eeb45ad5063c388a4fdf6201fee634d1d8
Jacek <jacek@suchenia.pl>
2023-12-13 09:00:38 +0100

    Day 12 - improve loop, break it

--- 2023/12/hot_springs_test.py
+++ 2023/12/hot_springs_test.py
@@ -1,7 +1,6 @@
 # Hot Springs - https://adventofcode.com/2023/day/12
 from functools import cache
 
-
 def get_data(filename: str):
     with open(filename) as f:
         results = []
@@ -11,7 +10,6 @@ def get_data(filename: str):
             results.append((data, nums))
 
         return results
-
 
 @cache
 def check_line(line: str, nums):
@@ -24,11 +22,11 @@ def check_line(line: str, nums):
     possible_windows = len(line) - needed_springs - needed_dots + 1  # At least one block
     for i in range(possible_windows):
         before = line[:i]
-        block = line[i : i + block_size]
-        after = line[i + block_size :]
+        block = line[i: i + block_size]
+        after = line[i + block_size:]
 
         if "#" in before:
-            continue
+            break
 
         if "." in block:
             continue
@@ -39,26 +37,21 @@ def check_line(line: str, nums):
         results += check_line(after[1:], nums[1:])
     return results
 
-
 def q1(filename: str) -> int:
     data = get_data(filename)
     return sum([check_line(line=line, nums=nums) for line, nums in data])
-
 
 def q2(filename: str) -> int:
     data = get_data(filename)
     return sum([check_line(line="?".join([line] * 5), nums=nums * 5) for line, nums in data])
 
-
 def test_line():
     assert check_line("???.###", (1, 1, 3)) == 1
     assert check_line(".??..??...?##.", (1, 1, 3)) == 4
 
-
 def test_q1():
     assert q1("test.txt") == 21
     assert q1("data.txt") == 7379
-
 
 def test_q2():
     assert q2("test.txt") == 525152
