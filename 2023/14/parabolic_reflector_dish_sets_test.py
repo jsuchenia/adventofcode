94ded0ce9fdc44ee09e24b791fab3d19f3f2f800
Jacek <jacek@suchenia.pl>
2023-12-14 11:26:40 +0100

    Day 14 - platform has one size

--- 2023/14/parabolic_reflector_dish_sets_test.py
+++ 2023/14/parabolic_reflector_dish_sets_test.py
@@ -9,17 +9,17 @@ def get_data(filename: str) -> dict[Rock, str]:
                 for x, c in enumerate(line)
                 if c != '.'}
 
-def move_rocks(cube_shaped: frozenset[Rock], rounded: frozenset[Rock], max_rows: int, max_cols: int, direction: Rock):
+def move_rocks(cube_shaped: frozenset[Rock], rounded: frozenset[Rock], size: int, direction: Rock):
     while True:
         new_rounded = set()
         for x, y in rounded:
-            rock = nx, ny = x + direction[0], y + direction[1]
-            if not (0 <= nx <= max_cols and 0 <= ny <= max_rows):
+            nx, ny = x + direction[0], y + direction[1]
+            if not (0 <= nx <= size and 0 <= ny <= size):
                 new_rounded.add((x, y))
-            elif rock in rounded or rock in cube_shaped:
+            elif (nx, ny) in rounded or (nx, ny) in cube_shaped:
                 new_rounded.add((x, y))
             else:
-                new_rounded.add(rock)
+                new_rounded.add((nx, ny))
 
         if new_rounded == rounded:
             break
@@ -29,36 +29,34 @@ def move_rocks(cube_shaped: frozenset[Rock], rounded: frozenset[Rock], max_rows:
 
 def q1(filename: str) -> int:
     platform = get_data(filename)
-    max_rows = max(y for x, y in platform)
-    max_cols = max(x for x, y in platform)
+    size = max(max(x, y) for x, y in platform)
 
     cube_shaped = frozenset(k for k, v in platform.items() if v == "#")
     rounded_rocks = frozenset(k for k, v in platform.items() if v == "O")
 
-    rounded_rocks = move_rocks(cube_shaped, rounded_rocks, max_rows, max_cols, (0, -1))
-    return sum(max_rows - y + 1 for x, y in rounded_rocks)
+    rounded_rocks = move_rocks(cube_shaped, rounded_rocks, size, (0, -1))
+    return sum(size - y + 1 for x, y in rounded_rocks)
 
 def q2(filename: str, rounds=1_000_000_000) -> int:
     platform = get_data(filename)
-    max_rows = max(y for x, y in platform)
-    max_cols = max(x for x, y in platform)
+    size = max(max(x, y) for x, y in platform)
 
     cube_shaped = frozenset(k for k, v in platform.items() if v == "#")
     rounded_rocks = frozenset(k for k, v in platform.items() if v == "O")
 
     state = {}
     while rounds > 0:
-        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, max_rows, max_cols, (0, -1))
-        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, max_rows, max_cols, (-1, 0))
-        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, max_rows, max_cols, (0, 1))
-        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, max_rows, max_cols, (1, 0))
+        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, size, (0, -1))
+        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, size, (-1, 0))
+        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, size, (0, 1))
+        rounded_rocks = move_rocks(cube_shaped, rounded_rocks, size, (1, 0))
         rounds -= 1
 
         if rounded_rocks in state and rounds % (state[rounded_rocks] - rounds) == 0:
             break
         state[rounded_rocks] = rounds
 
-    return sum(max_rows - y + 1 for x, y in rounded_rocks)
+    return sum(size - y + 1 for x, y in rounded_rocks)
 
 def test_q1():
     assert q1("test.txt") == 136
