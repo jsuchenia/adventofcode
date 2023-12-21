ecdc3c1c27a4d0f6e018819b902032de0d5f9586
Jacek <jacek@suchenia.pl>
2023-12-21 23:21:00 +0100

    Day 21 - Add WA suggestion

--- 2023/21/step_counter_test.py
+++ 2023/21/step_counter_test.py
@@ -53,27 +53,33 @@ def q2(filename="data.txt", steps=26501365) -> int:
     # From wolfram alpha: 26501365 and 131 - no relations, only 26501365 = 202300 × 131 + 65
 
     size = len(data)
-    n = steps // size  # 202300 - we have samples, each with 131 step
+    n = steps // size  # 202300 - 2023*100 - probably some clue - we have samples, each with 131 step
     p = steps % size  # 65 - also magic value as it's a half of a size of a grid
 
-    val0 = q1(filename, steps=p)  # 3742
+    val0 = 3742  # q1(filename, steps=p)  # 3742
     val1 = 33564  # q1(filename, steps=1 * size + p)  # 33564 (~2 sec)
     val2 = 93148  # q1(filename, steps=2 * size + p)  # 93148 (~13 sec)
     val3 = 182494  # q1(filename, steps=3 * size + p)  # 182494 (~3 minutes...)
+    val4 = 301602  # From WA
+
     # val202300 - target
-    print(f"{val0=} {val1=} {val2=} {val3=}")
+    # print(f"{val0=} {val1=} {val2=} {val3=} {val4=}")
 
     diff1 = val1 - val0  # 29822
     diff2 = val2 - val1  # 59584 = val0 + 1*ddiff
     diff3 = val3 - val2  # 89346 = val0 + 2*ddiff
-    print(f"DIFF {val1=} {diff1=} {diff2=} {diff3=}")
+    diff4 = val4 - val3  # 119108 = val0 + 3*ddiff
+    # print(f"DIFF {val1=} {diff1=} {diff2=} {diff3=} {diff4=}")
 
     ddiff1 = diff2 - diff1  # 29762
     ddiff2 = diff3 - diff2  # 29762 # BINGO!!
-    print(f"DIFF OF DIFF {ddiff1=} {ddiff2=}")
+    ddiff3 = diff4 - diff3  # 29762 # BINGO!!
+    # print(f"DIFF OF DIFF {ddiff1=} {ddiff2=}")
 
-    # assert ddiff1 == ddiff2
-    # assert diff3 - diff1 == 2 * ddiff1
+    assert ddiff1 == ddiff2
+    assert ddiff2 == ddiff3
+    assert diff3 - diff1 == 2 * ddiff1
+    assert diff4 - diff1 == 3 * ddiff1
 
     # diff = 29762*x + 29822
     # Linear function of delta (where x is per each 131 block - value is a real value)
@@ -84,26 +90,40 @@ def q2(filename="data.txt", steps=26501365) -> int:
 
     return sum(diff(n)) + val0
 
+def q2_wa(filename="data.txt", steps=26501365) -> int:
+    data = get_data(filename)
+    size = len(data)
+    n = steps // size
+
+    # Math proposed by WA from 4 samples: https://www.wolframalpha.com/input?i=3742%2C33564%2C93148%2C182494
+    # WA is indexing elements from 1, so we have to calculate it for n+1
+
+    wa = lambda x: 14881 * x ** 2 - 14821 * x + 3682
+    return wa(n + 1)
+
 def test_q1():
     assert q1("test.txt", 6) == 16
     assert q1("data.txt", 64) == 3682
 
-def test_q1_nostrict():
+def test_q2_example_values():
     assert q1("test.txt", 6) == 16
     assert q1("test.txt", 10) == 50
     assert q1("test.txt", 50) == 1594
     assert q1("test.txt", 100) == 6536
-    # too complex to simulate..
+    # too complex to simulate on my laptop
     # assert q1("test.txt", 500) == 167004
     # assert q1("test.txt", 1000) == 668697
     # assert q1("test.txt", 5000) == 16733044
 
-@pytest.mark.skip
-def test_q2_data_nostrict(filename="data.txt", steps=26501365):
+def test_q2():
+    assert q2() == 609012263058042
+    assert q2_wa() == 609012263058042
+
+@pytest.mark.skip("Used to generate samples for a discovery..")
+def test_q2_data(filename="data.txt", steps=26501365):
     data = get_data(filename)
 
     size = len(data)
-    n = steps // size  # 202300
     p = steps % size  # 65
 
     assert q1("data.txt", steps=p) == 3742  # 96ms
@@ -111,7 +131,7 @@ def test_q2_data_nostrict(filename="data.txt", steps=26501365):
     assert q1("data.txt", steps=2 * size + p) == 93148  # 13 sec 135ms
     assert q1("data.txt", steps=3 * size + p) == 182494  # (3 minutes...)
 
-@pytest.mark.skip
+@pytest.mark.skip("Used during work on p2")
 def test_plot_points():
     q1_points = [(6, 16), (10, 50), (50, 1594), (100, 6536), (500, 167004), (1000, 668697), (5000, 16733044)]
 
@@ -138,6 +158,3 @@ def test_plot_points():
     plt.grid(True)
     plt.ticklabel_format(useOffset=False, style='plain')
     plt.savefig("test.txt.png")
-
-def test_q2():
-    assert q2() == 609012263058042
