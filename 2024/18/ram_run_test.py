a3444d9222293f44cea4580b9cffd324176b877d
Jacek <jacek@suchenia.pl>
2024-12-18 07:54:53 +0100

    AoC 2024 - Day 18

---
+++ 2024/18/ram_run_test.py
@@ -0,0 +1,62 @@
+# RAM Run - https://adventofcode.com/2024/day/18
+from networkx import shortest_path_length, Graph, shortest_path, NetworkXNoPath
+
+from aoclib import *
+
+
+def get_data(filename: str) -> list[complex]:
+    with open(filename) as f:
+        nums = [ints(line) for line in f.read().strip().splitlines()]
+    return [num[0] + 1j * num[1] for num in nums]
+
+
+def q1(filename: str, limit: int, end: int) -> int:
+    points = set(get_data(filename)[:limit])
+
+    g = Graph()
+
+    for y in range(end + 1):
+        for x in range(end + 1):
+            if (point := x + 1j * y) not in points:
+                g.add_node(point)
+                for direction in DIRECTIONS_4:
+                    if (new_point := point + direction) not in points and new_point in g:
+                        g.add_edge(point, new_point)
+
+    return shortest_path_length(g, 0, end + end * 1j)
+
+
+def q2(filename: str, end: int) -> str:
+    points = get_data(filename)
+
+    g = Graph()
+
+    for y in range(end + 1):
+        for x in range(end + 1):
+            g.add_node(point := x + 1j * y)
+            for direction in DIRECTIONS_4:
+                if (new_point := point + direction) in g:
+                    g.add_edge(point, new_point)
+
+    last_path = set(shortest_path(g, 0, (end_point := end + 1j * end)))
+    for point in points:
+        g.remove_node(point)
+        if point not in last_path:
+            continue
+
+        try:
+            last_path = set(shortest_path(g, 0, end_point))
+        except NetworkXNoPath:
+            return f"{int(point.real)},{int(point.imag)}"
+
+    raise ValueError("No such point on a list")
+
+
+def test_q1():
+    assert q1("test.txt", 12, 6) == 22
+    assert q1("data.txt", 1024, 70) == 322
+
+
+def test_q2():
+    assert q2("test.txt", 6) == "6,1"
+    assert q2("data.txt", 70) == "60,21"
