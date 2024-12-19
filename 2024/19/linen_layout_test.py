a7238f79b9f4dd8dd4ccf98068189e2dce2579f1
Jacek <jacek@suchenia.pl>
2024-12-19 08:14:38 +0100

    AoC 2024 - Day 19

---
+++ 2024/19/linen_layout_test.py
@@ -0,0 +1,59 @@
+# Linen Layout - https://adventofcode.com/2024/day/19
+from functools import cache
+
+
+def get_data(filename: str) -> tuple[list[str], list[str]]:
+    with open(filename) as f:
+        towels, patterns = lines = f.read().strip().split("\n\n")
+
+    towels = towels.strip().split(',')
+    towels = [towel.strip() for towel in towels]
+    patterns = patterns.strip().splitlines()
+    return towels, patterns
+
+
+def q1(filename: str) -> int:
+    towels, patterns = get_data(filename)
+    towels = set(towels)
+    result = 0
+
+    print(f"{towels=} {patterns=}")
+
+    @cache
+    def check(pattern: str) -> bool:
+        for towel in towels:
+            if pattern.startswith(towel):
+                if not (rest := pattern[len(towel):]) or check(rest):
+                    return True
+        return False
+
+    return sum(check(pattern) for pattern in patterns)
+
+
+def q2(filename: str) -> int:
+    towels, patterns = get_data(filename)
+    towels = set(towels)
+
+    @cache
+    def check(pattern: str) -> int:
+        result = 0
+
+        for towel in towels:
+            if pattern.startswith(towel):
+                if rest := pattern[len(towel):]:
+                    result += check(rest)
+                else:
+                    result += 1
+        return result
+
+    return sum(check(pattern) for pattern in patterns)
+
+
+def test_q1():
+    assert q1("test.txt") == 6
+    assert q1("data.txt") == 358
+
+
+def test_q2():
+    assert q2("test.txt") == 16
+    assert q2("data.txt") == 600_639_829_400_603
